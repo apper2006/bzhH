3dd4f7f0781c34d675ee8c35a5540cc5b9824de1
apper2006 <85793970+apper2006@users.noreply.github.com>
2022-04-18 09:13:16 +0530

    Add files via upload

---
+++ nft.js
@@ -0,0 +1,18 @@
+{
+    "title": "Asset Metadata",
+    "type": "object",
+    "properties": {
+        "name": {
+            "type": "string",
+            "description": "POLYGON"
+        },
+        "description": {
+            "type": "string",
+            "description": "MY FIRST NFT"
+        },
+        "image": {
+            "type": "string",
+            "description": "https://ibb.co/1sZrfcm"
+        }
+    }
+}
